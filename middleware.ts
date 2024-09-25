258220495f02277af8e61cfbe43585d6ef8b40e4
fangdehui <javaeecoding@gmail.com>
2024-09-25 12:43:30 +0900

    add E parttern

--- middleware.ts
+++ middleware.ts
@@ -26,19 +26,45 @@ export function middleware(req: NextRequest) {
     if (isMobile) {
       const url = req.nextUrl.clone()
       url.pathname = `/p/sp/${id}`
+      console.log("P rewrite from,to=", req.nextUrl.pathname, url.pathname);
       return NextResponse.rewrite(url);
     } else {
       const url = req.nextUrl.clone()
       url.pathname = `/p/pc/${id}`
+      console.log("P rewrite from,to=", req.nextUrl.pathname, url.pathname);
       return NextResponse.rewrite(url);
     }
   }
 
+  // Check if the request is for a path that starts with '/e/'
+  if (pathname.startsWith('/e/') && !pathname.endsWith('/sp/') && !pathname.endsWith('/pc/')) {
+    // Extract the 'id' from the URL path
+    const id = pathname.split('/e/')[1];
+
+    // Check User-Agent
+    const userAgent = req.headers.get('user-agent') || '';
+
+    // Regex to check if UA is mobile
+    const isMobile = /mobile|android|iphone|ipad|phone/i.test(userAgent);
+
+    if (isMobile) {
+      const url = req.nextUrl.clone()
+      url.pathname = `/e/${id}/sp/`
+      console.log("E rewrite from,to=", req.nextUrl.pathname, url.pathname);
+      return NextResponse.rewrite(url);
+    } else {
+      const url = req.nextUrl.clone()
+      url.pathname = `/e/${id}/pc/`
+      console.log("E rewrite from,to=", req.nextUrl.pathname, url.pathname);
+      return NextResponse.rewrite(url);
+    }
+  }
+  
   // If the path is not matched, proceed with the request
   return NextResponse.next();
 }
 
 // Specify the paths where the middleware should be applied
 export const config = {
-  matcher: ['/p/:id*'],
+  matcher: ['/p/:id*', '/e/:id*'],
 };
